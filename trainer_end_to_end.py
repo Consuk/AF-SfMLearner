e80945532dfbe31a98b72fd01769db00b5d26f0b
Consuk <0228053@up.edu.mx>
2025-07-20 18:31:49 -0600

    Versión corregida del trainer compatible con docker

--- trainer_end_to_end.py
+++ trainer_end_to_end.py
@@ -139,7 +139,7 @@ class Trainer:
             self.opt.frame_ids, 4, is_train=True, img_ext=img_ext)
         self.train_loader = DataLoader(
             train_dataset, self.opt.batch_size, True,
-            num_workers=self.opt.num_workers, pin_memory=True, drop_last=True)
+            num_workers=0, pin_memory=True, drop_last=True)
         val_dataset = self.dataset(
             self.opt.data_path, val_filenames, self.opt.height, self.opt.width,
             self.opt.frame_ids, 4, is_train=False, img_ext=img_ext)
