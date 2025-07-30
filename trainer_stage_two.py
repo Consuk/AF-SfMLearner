99bf0affa631272101312bf619f63c6d567c9c2f
Consuk <0228053@up.edu.mx>
2025-07-29 18:36:51 -0600

    lowering batches

--- trainer_stage_two.py
+++ trainer_stage_two.py
@@ -16,6 +16,8 @@ from tensorboardX import SummaryWriter
 
 class Trainer:
     def __init__(self, options):
+        self.opt.batch_size = 1
+
         self.opt = options
         self.log_path = os.path.join(self.opt.log_dir, self.opt.model_name)
 
